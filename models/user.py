3c11e2a796f6ca30bdd089d44256a514d0995088
Abdul Rahman <abdulnsamba@gmail.com>
2023-08-14 01:02:31 +0300

    user

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    This is the User Model
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Attribues and Methods in the User Model"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self):
+        """ Initialize the User Mode using the Base"""
+        super().__init__()
