9c2e1c8d09618036e6401574cf1f7a2e55721e55
Abdul Rahman <abdulnsamba@gmail.com>
2023-08-14 01:33:13 +0300

    other cls

--- models/user.py
+++ models/user.py
@@ -13,7 +13,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self):
-        """ Initialize the User Mode using the Base"""
-        super().__init__()
